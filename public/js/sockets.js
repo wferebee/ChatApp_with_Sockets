4abc4275a374557ebf47ddbcccd59c4a9e8163c3
DP3PO <williampferebee@gmail.com>
2021-02-13 14:10:53 -0500

    updated socket logic to use JQuery jut to be consistent, still more to do. have to take a break so leaving off here

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -2,28 +2,29 @@ $(function () {
     
     var socket = io();
      setUsername =()=>{
-       socket.emit('setUsername', document.getElementById('name').value);
+       socket.emit('setUsername', $("#name").val());
+       
     };
     var user;
     socket.on('userExists', function(data) {
-       document.getElementById('error-container').innerHTML = data;
+      $('#error-container').html(data);
     });
     socket.on('userSet', function(data) {
        user = data.username;
-       document.body.innerHTML = '<input type = "text" id = "message">\
+       $('body').html('<input type = "text" id = "message">\
        <button type = "button" name = "button" onclick = "sendMessage()">Send</button>\
-       <div id = "message-container"></div>';
+       <div id = "message-container"></div>');
     });
     sendMessage = () => {
-       var msg = document.getElementById('message').value;
+       var msg = $('#message').val();
        if(msg) {
           socket.emit('msg', {message: msg, user: user});
        }
     }
     socket.on('newmsg', function(data) {
        if(user) {
-          document.getElementById('message-container').innerHTML += '<div><b>' + 
-             data.user + '</b>: ' + data.message + '</div>'
+          $('#message-container').append('<div><b>' + 
+          data.user + '</b>: ' + data.message + '</div>');
        }
     })
     
