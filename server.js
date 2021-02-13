99a6ff7b51bb41866afd232fce912af3fca3935c
DP3PO <williampferebee@gmail.com>
2021-02-13 12:01:39 -0500

    added the sockets dependency and set up inital 'connect /disconnect' protocol, also added the socket cdn? and instantiation on index.html

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const path = require('path'); // to serve specific files whennroutes are hit
-const morgan = require('morgan'); // error middleware
+const morgan = require('morgan');
 const app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
 app.use(express.static('public')); // to serve static files
-app.use(morgan('dev')); // 'dev' is just the paramter that Type/ URL/ Status-Code/ Time-Elapsed
-
+app.use(morgan('dev'));
 var PORT = process.env.PORT || 3000; // PORT = 3000 or whatever the enviornmanet sets it to
+
 
 app.get("/", (req, res, next) => { // Just setting up to route to the main index page
 
     res.sendFile(path.join(__dirname, '/public', '/html', 'index.html'));
 })
 
-app.get("/extra", (req, res, next) => { // route to extra page
 
-    res.sendFile(path.join(__dirname, '/public', '/html', 'extra.html'));
-})
+io.on('connection', function(socket) {
+    console.log('\n A user connected \n');
+ 
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+       console.log('\n A user disconnected \n');
+    });
+ });
 
-app.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
+
+
+ http.listen(PORT, () => {
+    console.log('listening on *:3000');
+ });
