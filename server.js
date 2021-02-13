ee2946db3ca57b78527d9541757175f7f0a1c19b
DP3PO <williampferebee@gmail.com>
2021-02-13 13:45:38 -0500

    set up inital chat application and wrapped socket logic in a JQ on load function. need to fix up html, then add database for users

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res, next) => { // Just setting up to route to the main index
 })
 
 
-var clients = 0;
+/* var clients = 0;
 io.on('connection', function(socket) {
    clients++;
    socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
@@ -28,7 +28,27 @@ io.on('connection', function(socket) {
       socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
    });
 });
+ */
 
+users = [];
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(data);
+      
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   
+   socket.on('msg', function(data) {
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
 
 
  http.listen(PORT, () => {
