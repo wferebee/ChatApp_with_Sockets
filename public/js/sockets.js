d81f3450e6de15ac2ae3fefdd2e71b140485aaf5
DP3PO <williampferebee@gmail.com>
2021-02-20 15:52:40 -0500

    changed a few things took away mongodb for users because i dont need that for such a small applcation

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,35 +1,60 @@
 $(function () {
-    
-    var socket = io();
-     setUsername =()=>{
-       socket.emit('setUsername', $("#name").val());
-       
-    };
-    var user;
-    socket.on('userExists', function(data) {
+
+   var socket = io();
+   setUsername = () => {
+      socket.emit('setUsername', $("#name").val());
+
+   };
+   var user;
+   socket.on('userExists', function (data) {
       $('#error-container').html(data);
-      
-    });
-    socket.on('userSet', function(data) {
-       user = data.username;
-       $('#username').hide();
-       $('#error-container').html(`<p>Username: ${user}</p>`)
-       $('#textAreaCol').html('<textarea type = "text" id = "message"></textarea>\
-       <button type = "button" name = "button" onclick = "sendMessage()">Send</button>\
-       <div id = "message-container"></div>');
-    });
-    sendMessage = () => {
-       var msg = $('#message').val();
-       if(msg) {
-          socket.emit('msg', {message: msg, user: user});
-       }
-    }
-    socket.on('newmsg', function(data) {
-       if(user) {
-          $('#message-container').append('<div><b>' + 
-          data.user + '</b>: ' + data.message + '</div>');
-       }
-    })
-    
-    
-      });
+
+   });
+   socket.on('userSet', function (data) {
+      user = data.username;
+      $('#username').hide();
+      $('#error-container').html(`<p>Username: ${user}</p>`)
+      $('#chatText').html('<textarea type = "text" id = "message"></textarea>\
+       <button id="textButton" type = "button" name = "button" onclick = "sendMessage()">Send</button>');
+
+      $("#textAreaCol").html('<div id = "message-container"></div>')
+   });
+   sendMessage = () => {
+      var msg = $('#message').val();
+      if (msg) {
+         socket.emit('msg', {
+            message: msg,
+            user: user
+         });
+         $('#message').val(" ");
+      }
+   }
+   socket.on('newmsg', function (data) {
+      if (user) {
+         if($("#message").val("") === ''){
+            /* nothing */
+         }
+         $('#message-container').append('<p><b>' +
+            data.user + '</b>: ' + data.message + '</p>');
+         var objDiv = $("#message-container");
+         objDiv.scrollTop = objDiv.scrollHeight;
+      }
+   })
+
+   socket.on('newclientconnect', function (data) {
+
+      $("#inRoom").html(`<p> ${data.description} </p>`);
+
+   });
+
+
+   function setFocusOnDivWithId(elementId) {
+      const scrollIntoViewOptions = {
+         behavior: "smooth",
+         block: "center"
+      };
+      document.getElementById(elementId).scrollIntoView(scrollIntoViewOptions);
+   };
+   setFocusOnDivWithId("#message-container");
+
+});
--- server.js
+++ server.js
@@ -3,16 +3,8 @@ const path = require('path'); // to serve specific files whennroutes are hit
 const morgan = require('morgan');
 const app = express();
 
-const mongojs = require("mongojs");
-/*   ---------------------------------------------------- */
-var databaseUrl = "chatUsers";
-var collections = ["users"];
-var db = mongojs(databaseUrl, collections);
 
-db.on("error", function(error) {
-   console.log("Database Error:", error);
-   
- });
+
 
 
 
@@ -32,78 +24,46 @@ var PORT = process.env.PORT || 3000; // PORT = 3000 or whatever the enviornmanet
 
 
 app.get("/", (req, res, next) => { // Just setting up to route to the main index page
-   db.users.find({}, function(err, found) {
-      // Log any errors if the server encounters one
-      if (err) {
-        console.log(err);
-      }
-      // Otherwise, send the result of this query to the browser
-      else {
-       console.log((found));
-
-       for (const person in found) {
-          if (Object.hasOwnProperty.call(found, person)) {
-             const qPerson = found[person];
-             console.log(qPerson.firstName)
-          }
-       }
-      }
-    });
 
 
-    res.sendFile(path.join(__dirname, '/public', '/html', 'index.html'));
+
+   res.sendFile(path.join(__dirname, '/public', '/html', 'index.html'));
 })
 
 
-/* var clients = 0;
-io.on('connection', function(socket) {
-   clients++;
-   socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
-   socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
-   socket.on('disconnect', function () {
-      clients--;
-      socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
-   });
-});
- */
-const inserting = (username) => {
-db.users.insert({"firstName": `${username}`}, function(err, found) {
-   // Log any errors if the server encounters one
-   if (err) {
-     console.log(err);
-   }
-   // Otherwise, send the result of this query to the browser
-   else {
-    
 
-    for (const person in found) {
-       if (Object.hasOwnProperty.call(found, person)) {
-          const qPerson = found[person];
-          console.log(qPerson.firstName)
-       }
-    }
-   }
- })
-}
-
- 
-
+var clients = 0;
 users = [];
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
    console.log('A user connected');
-   socket.on('setUsername', function(data) {
+   socket.on('setUsername', function (data) {
       console.log(data);
-      
-      if(users.indexOf(data) > -1) {
+
+      if (users.indexOf(data) > -1) {
          socket.emit('userExists', data + ' username is taken! Try some other username.');
       } else {
          users.push(data);
-         socket.emit('userSet', {username: data});
-         inserting(data);
+         socket.emit('userSet', {
+            username: data
+         });
+         clients++;
+         socket.emit('newclientconnect', {
+            description: 'Hey, welcome!'
+         });
+         socket.broadcast.emit('newclientconnect', {
+            description: clients + ' clients connected!'
+         })
+         socket.on('disconnect', function () {
+            clients--;
+            socket.broadcast.emit('newclientconnect', {
+               description: clients + ' clients connected!'
+            })
+         });
+
       }
    });
-   
-   socket.on('msg', function(data) {
+
+   socket.on('msg', function (data) {
       //Send message to everyone
       io.sockets.emit('newmsg', data);
    })
@@ -112,6 +72,6 @@ io.on('connection', function(socket) {
 
 
 
- http.listen(PORT, () => {
-    console.log('listening on *:3000');
- });
+http.listen(PORT, () => {
+   console.log('listening on *:3000');
+});
