f81f4d87222fcd8e8f535225ce860776f5aaea75
William Ferebee <williampferebee@gmail.com>
2021-08-06 13:39:43 -0400

    changed some stuff up and added somestyling

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const path = require('path'); // to serve specific files whennroutes are hit
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser')
 const userRouter = require('./src/routers/user')
-const PORT = process.env.PORT
+const PORT = 4000
 require('./src/db/db')
+require('dotenv').config()
 
 const app = express();
 const http = require('http').Server(app);
@@ -19,5 +20,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
 
 http.listen(PORT, () => {
-   console.log('listening on *:3000');
+   console.log(`listening on ${PORT}`);
 });
--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,8 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+mongoose.connect("mongodb://localhost:27017/users", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
